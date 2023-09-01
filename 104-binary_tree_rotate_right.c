32a861d70b159bcd684dd9e669a73d2766829a83
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-09-01 10:48:02 -0700

    last up date

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -8,24 +8,22 @@
 
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	binary_tree_t *new_root, *new_root_right_child, *_parent;
+	binary_tree_t *new_root, *new_root_right_child, *parent;
 
 	if (tree == NULL || tree->left == NULL)
 		return (tree);
 
-	/*storing the ref of the nodes invloved */
-	_parent = tree;
+	parent = tree;
 	new_root = tree->left;
 	new_root_right_child = new_root->right;
 
-	/* rotating right */
-	_parent->left = new_root_right_child;
+	parent->left = new_root_right_child;
 	if (new_root_right_child != NULL)
-		new_root_right_child->_parent = _parent;
+		new_root_right_child->parent = parent;
 
-	new_root->right = _parent;
-	new_root->_parent = _parent->_parent;
-	_parent->_parent = new_root;
+	new_root->right = parent;
+	new_root->parent = parent->parent;
+	parent->parent = new_root;
 
 	return (new_root);
 }
