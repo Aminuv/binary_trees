2adc73815149f049c28ce43c6ce7dd56f5374a73
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-09-01 07:30:04 -0700

    againe testing

--- binary_trees.h
+++ binary_trees.h
@@ -70,7 +70,6 @@ heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
-binary_tree_t *sib_ling(binary_tree_t *node)²:x
 
 
 
