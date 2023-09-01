c6d0b9edade941713db8ee076f1a438e5a16deb9
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-09-01 15:43:44 -0700

    tasks 120

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,35 +1,66 @@
 #include "binary_trees.h"
+#include <limits.h>
 
 /**
- * binary_tree_is_leaf -  The checker for the node in leaf.
- * @node: is a pointer of the node for checker.
+ * height - A height of the tree
+ * @tree: the tree
  *
- * Return: return 1, or 0.
+ * Return: the height of it.
  */
-int binary_tree_is_leaf(const binary_tree_t *node)
+int height(const binary_tree_t *tree)
 {
-	if (node == NULL)
-		return (0);
+	int left_height, right_height;
 
-	if (node->right == NULL && node->left == NULL)
-		return (1);
-	else
+	if (tree == NULL)
 		return (0);
+	left_height = height(tree->left);
+	right_height = height(tree->right);
+
+	return (1 + ((left_height > right_height) ? left_height : right_height));
 }
 
 /**
- * binary_tree_leaves - The number of the leaves.
- * @tree: A pointer to the root node.
+ * is_bst - The checks if the binary tree is a 'BST'.
+ * @tree: A pointer to the root node
+ * @prev: A pointer to the int holding traversed value
  *
- * Return: NULL, or 0.
+ * Return: 1 or, 0 if is NULL.
  */
-size_t binary_tree_leaves(const binary_tree_t *tree)
+int is_bst(const binary_tree_t *tree, int *prev)
 {
 	if (tree == NULL)
+		return (1);
+	if (!is_bst(tree->left, prev))
 		return (0);
-	else
-		return (binary_tree_is_leaf(tree) +
-			binary_tree_leaves(tree->right) +
-			binary_tree_leaves(tree->left));
+	if (*prev >= tree->n)
+		return (0);
 
+	*prev = tree->n;
+
+	return (is_bst(tree->right, prev));
+}
+
+/**
+ * binary_tree_is_avl - the checks if a BT is an AVL tree
+ * @tree: A pointer to the root node
+ *
+ * Return: 1 or, 0 on failure.
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	int leftheight, rightheight, balancefactor;
+	int prev = INT_MIN;
+
+	if (tree == NULL)
+		return (0);
+
+	if (!is_bst(tree, &prev))
+		return (0);
+
+	leftheight = height(tree->left);
+	rightheight = height(tree->right);
+	balancefactor = leftheight - rightheight;
+
+	return ((balancefactor >= -1 && balancefactor <= 1) &&
+		binary_tree_is_avl(tree->left) && binary_tree_is_avl(tree->right));
 }
